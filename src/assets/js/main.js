e6e65a0c9fdecb5b2d8de8ae2794f9664cc947fd
Emmanuel Gomez <gomezgtzemmanuel@gmail.com>
2021-10-01 16:21:46 -0500

    Aprendiendo npm

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+import Swal from '../../../node_modules/sweetalert2/src/sweetalert2.js';
+
+Swal.fire({
+  title: "Error!",
+  text: "Do you want to continue",
+  icon: "error",
+  confirmButtonText: "Cool",
+});
